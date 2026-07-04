71ab998a491e41766e8c96576cc7561b3db8e5ee
Gee Chong Shek <geeshek95@outlook.com>
2026-07-04 15:01:38 +0100

    first commit

---
+++ src/main/java/com/gee/springkafka/SpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.gee.springkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaApplication.class, args);
+	}
+
+}
